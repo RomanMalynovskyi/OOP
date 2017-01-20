8b991e96a31a5ccb02b510c090b8705c5ea079ec
RomanMalynovskyi <malinovskiir@mail.ru>
2017-01-20 23:44:22 +0200

    11

---
+++ src/com/rmal/javaOOP/homework/Board/Shape.java
@@ -0,0 +1,12 @@
+package com.rmal.javaOOP.homework.Board;
+
+/*1.Создайте абстрактный класс Shape в котором есть два
+абстрактных метода double getPerimetr() и double getArea().*/
+
+abstract class Shape {
+
+    public abstract double getPerimeter();
+
+    public abstract double getArea();
+
+}
